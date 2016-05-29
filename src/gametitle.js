d4d186bf27a2821c33b227df874ebea48ae1be70
Urian <urlee@ucsc.edu>
2016-05-29 08:42:16 +0000

    game camera resets now

--- src/gametitle.js
+++ src/gametitle.js
@@ -1,13 +1,14 @@
 var gameTitle = function(game){
-	p = null;
-	player = null;
-	temperature_reading = null;
-	startingTemp = 65;
-	w = 640;
-	h = 320;
-	ground = null;
+	title_p = null;
+	title_player = null;
+	title_temperature_reading = null;
+	title_startingTemp = 65;
+	title_w = 640;
+	title_h = 320;
+	title_ground = null;
 	var groundLength = 15;
-	wc = null;
+	level0 = 690;
+	title_wc = null;
 	
 	// Sounds
 	sound_shoot = null;
@@ -30,11 +31,11 @@ gameTitle.prototype = {
     	var background = this.game.add.tileSprite(0, 0, 4200, 720, "background");
     	
 		// CREDITS PAGE
-		var Title = this.game.add.bitmapText(w+20, h, 'pixely_font', 'CREDITS', 24);
-    	var LCN = this.game.add.bitmapText(w+20, h+40, 'pixely_font', 'LUCIENNE LEE', 40);
-    	var ETN = this.game.add.bitmapText(w+20, h+100, 'pixely_font', 'ETHAN WONG', 40);
-    	var JAY = this.game.add.bitmapText(w+20, h+160, 'pixely_font', 'JAY PATEL', 40);
-    	var UUU = this.game.add.bitmapText(w+20, h+220, 'pixely_font', 'URIAN LEE', 40);
+		var Title = this.game.add.bitmapText(title_w+20, title_h, 'pixely_font', 'CREDITS', 24);
+    	var LCN = this.game.add.bitmapText(title_w+20, title_h+40, 'pixely_font', 'LUCIENNE LEE', 40);
+    	var ETN = this.game.add.bitmapText(title_w+20, title_h+100, 'pixely_font', 'ETHAN WONG', 40);
+    	var JAY = this.game.add.bitmapText(title_w+20, title_h+160, 'pixely_font', 'JAY PATEL', 40);
+    	var UUU = this.game.add.bitmapText(title_w+20, title_h+220, 'pixely_font', 'URIAN LEE', 40);
     	
     	//BACKGROUND 1
   		var background1 = this.game.add.tileSprite(0, 0, 4200, 720, "background1");
@@ -43,37 +44,37 @@ gameTitle.prototype = {
     	this.game.physics.startSystem(Phaser.Physics.ARCADE);
     	
     	// TEMPERATURE BECAUSE IT IS NEEDED TO CREATE PLAYER 
-		temperature_reading = this.game.add.text(this.game.camera.x+550, this.game.camera.y+50, startingTemp, {
+		title_temperature_reading = this.game.add.text(this.game.camera.x+550, this.game.camera.y+50, title_startingTemp, {
   			font: "1px Arial", //TOXICITY BAR
   			fill: "000000",
   			align: "center"
   		});
-		temperature_reading.temp = startingTemp;
-		temperature_reading.nTemp = startingTemp;
-		temperature_reading.anchor.setTo(0.5, 0.5);
-		temperature_reading.fixedToCamera = true;
+		title_temperature_reading.temp = title_startingTemp;
+		title_temperature_reading.nTemp = title_startingTemp;
+		title_temperature_reading.anchor.setTo(0.5, 0.5);
+		title_temperature_reading.fixedToCamera = true;
 		
 		// PLAYER CREATION
-		p = new Title_Player(this.game, temperature_reading);
-		p.create();
-		player = p.sprite;
+		title_p = new Title_Player(this.game, title_temperature_reading);
+		title_p.create();
+		title_player = title_p.sprite;
 		
 		// GROUND EXISTS
-		ground = this.game.add.physicsGroup();
+		title_ground = this.game.add.physicsGroup();
 		//E: use ground length to determine how far the ground goes
 		var groundLength = 8;
 		for (var i = 0; i < groundLength; i++) {
-			ground.create(300 * i, level0, 'ground'); // ground
+			title_ground.create(300 * i, level0, 'ground'); // ground
 			this.add.sprite(300 * i, level0-groundLength,"groundI");
 		}
-		ground.setAll('body.immovable', true);
-		ground.setAll('body.checkCollision.down', false);
-		ground.setAll('body.checkCollision.left', false);
-		ground.setAll('body.checkCollision.right', false);
+		title_ground.setAll('body.immovable', true);
+		title_ground.setAll('body.checkCollision.down', false);
+		title_ground.setAll('body.checkCollision.left', false);
+		title_ground.setAll('body.checkCollision.right', false);
 		
 		//WATERING CAN 
-		wc = new wateringcanShooter(this.game, player, ground, temperature_reading);
-		wc.create();
+		title_wc = new wateringcanShooter(this.game, title_player, title_ground, title_temperature_reading);
+		title_wc.create();
 		
 		// shoot sounds
 		sound_shoot = this.game.add.audio('shoot');
@@ -93,12 +94,12 @@ gameTitle.prototype = {
 	},
 	update: function() {
 		
-		this.game.physics.arcade.collide(player, ground); //PLAYER TOUCHES GROUND
-		p.update(); // PLAYER UPDATE
-		wc.update(); // WATERING CAN UPDATE
+		this.game.physics.arcade.collide(title_player, title_ground); //PLAYER TOUCHES GROUND
+		title_p.update(); // PLAYER UPDATE
+		title_wc.update(); // WATERING CAN UPDATE
 		
 		// ARROW KEYS TUTORIAL
-		if (p.didL && p.didU && p.didR) {
+		if (title_p.didL && title_p.didU && title_p.didR) {
 			if (m_arrowkeys.alpha < 0.2) {
 				m_arrowkeys.alpha = 0;
 				m_arrowkeys.setText('');
@@ -112,7 +113,7 @@ gameTitle.prototype = {
 		}
 
 		// SPACE BAR Completed
-		if (wc.didShoot) {
+		if (title_wc.didShoot) {
 			if (m_space.alpha < 0.1) {
 				m_space.alpha = 0;
 				m_space.setText('');
@@ -127,7 +128,8 @@ gameTitle.prototype = {
 				//m_space.scale += 0.5;
 			}
 		}
-		if (player.body.x >= 1200) {
+		if (title_player.body.x >= 1200) {
+			this.game.camera.reset();
 			this.game.state.start("TheGame");
 			
 		}
