4f2cd937df18a3850e5e195ecbe4b72987723394
Urian <urlee@ucsc.edu>
2016-05-30 09:22:47 +0000

    you won

--- src/stage_cleared.js
+++ src/stage_cleared.js
@@ -4,8 +4,7 @@ stage_cleared.prototype = {
 	init: function(score){
 	},
   	create: function(){
-  		var stageClearedTitle = this.game.add.sprite(320,160,"stagecleared");
-		stageClearedTitle.anchor.setTo(0.5,0.5);
+		var gameTitle = this.game.add.bitmapText(100, 160, 'pixely_font', 'YOU WON', 64);
 		var playButton = this.game.add.button(320,320,"play",this.playTheGame,this);
 		playButton.anchor.setTo(0.5,0.5);
 		var quitButton = this.game.add.button(320, 380, "quit", this.quitTheGame, this);
